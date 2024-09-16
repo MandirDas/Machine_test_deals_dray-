4a2655810c8d826de35aa6e2a68baa55844c85cf
MandirDas <dasmandir0@gmail.com>
2024-09-16 14:49:52 +0530

    key changes

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -26,11 +26,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     try {
       // await SecureStorage.setRegistrationStatus('Complete');
       final temporaryStorage = TemporaryStorage();
-      temporaryStorage.setRegistrationStatus('Complete');
       await ApiService.register(email, password, referralCode, widget.userId);
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
+      temporaryStorage.setRegistrationStatus('Complete');
     } catch (e) {
       // Handle error
       print('Registration error: $e');
@@ -44,73 +44,86 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Center(
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    height: 250,
+                    width: 200,
+                    child: Opacity(
+                      opacity: 0.5,
+                      child: Image.asset(
+                        'assets/dealsdray_logo.jpeg',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Lets Begin!',
+                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Please enter your credentials to proceed',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                ),
+                TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: _referralCodeController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      labelText: 'Referral Code(Optional)'),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+            Positioned(
+              // left: 16,
+              right: 16,
+              bottom: 16,
               child: Container(
-                alignment: Alignment.topCenter,
-                height: 250,
-                width: 200,
-                child: Opacity(
-                  opacity: 0.5,
-                  child: Image.asset(
-                    'assets/dealsdray_logo.jpeg',
-                    fit: BoxFit.fill,
-                  ),
+                width: 56, // Standard size for Material touch targets
+                height: 56,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius:
+                      BorderRadius.circular(8), // Slightly rounded corners
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Lets Begin!',
-              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Please enter your credentials to proceed',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-            ),
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              obscureText: _obscurePassword,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscurePassword ? Icons.visibility_off : Icons.visibility,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-                ),
-              ),
-            ),
-            TextField(
-              controller: _referralCodeController,
-              keyboardType: TextInputType.number,
-              decoration:
-                  const InputDecoration(labelText: 'Referral Code(Optional)'),
-            ),
-            const SizedBox(height: 16),
-            Center(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  minimumSize: Size(double.maxFinite, 50),
-                ),
-                onPressed: _register,
-                child: const Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_forward),
+                  onPressed: _register,
+                  color: Colors.white,
+                  iconSize: 28, // Slightly larger icon
                 ),
               ),
             ),
